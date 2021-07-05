fa88484c6b7662d77450d085b972f0abb163a287
sunu-nandakumar <appu1998.nandakumar@gmail.com>
2021-07-05 10:45:29 +0530

    refactoring the code acc to Dilip sir's

--- DeckOfCards.java
+++ DeckOfCards.java
@@ -5,16 +5,25 @@ import java.util.Set;
 
 public class DeckOfCards {
 
-	// method for creating the deck of 52 cards
+		// method for creating the deck of 52 cards
 	public static String[] setup(String[] suit, String[] rank, String[] cardCombo) {
-//First loop goes through the suit array 
+		Set<String> uniqueCards = new HashSet<String>();
+		//First loop goes through the suit array 
 		for (int i = 0; i < suit.length; i++) {
 			// loop goes through the rank array
 			for (int j = 0; j < rank.length; j++) {
 				cardCombo[rank.length * i + j] = suit[i] + rank[j];
 			}
 		}
-//		
+		int count = 0;
+		while (count < 52) {
+			uniqueCards.add(cardCombo[count]);
+			count++;
+		}
+		cardCombo = uniqueCards.toArray( new String[0] );
+		for(int i=0; i<cardCombo.length;i++) {
+			System.out.println(cardCombo[i]);
+		}
 		return cardCombo;
 	}
 
@@ -22,18 +31,13 @@ public class DeckOfCards {
 		String[] suit = { "Clubs", "Hearts", "Diamonds", "Spade" };
 		String[] rank = { "Ace", "Kings", "Queen", "Jack", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
 		String[] cardCombo = new String[52];
-		Set<String> uniqueCards = new HashSet<String>();
+	
 		// calling the setup method to create the deck
 		setup(suit, rank, cardCombo);
-// to check whether the elements are unique or not
-		int count = 0;
-		while (count < 52) {
-			uniqueCards.add(cardCombo[count]);
-			count++;
-		}
-
-		System.out.println(uniqueCards);
-		System.out.println(uniqueCards.size());
+		
+		
+		
+		
 	}
 
 }
