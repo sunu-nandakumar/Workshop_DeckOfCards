99baf483aee88247414bb36742783362334c5b9d
sunu-nandakumar <appu1998.nandakumar@gmail.com>
2021-07-05 09:39:23 +0530

    refactoring the first uc1 with the reviews from dilip

--- DeckOfCards.java
+++ DeckOfCards.java
@@ -2,23 +2,28 @@ package deckOfCards;
 
 public class DeckOfCards {
 
-	public static void setup() {
-		String[] suit = {"Clubs", "Hearts", "Diamonds", "Spade"};
-		String[] rank = { "Ace", "Kings", "Queen", "Jack", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
-		String[] cardCombo = new String [52];
-		
-		for(int i=0; i<4;i++) {
-			for(int j=0; j<13;j++) {
-				cardCombo[rank.length*i+j] = suit[i] + rank[j];
+	// method for creating the deck of 52 cards
+	public static String[] setup(String[] suit, String[] rank, String[] cardCombo) {
+//First loop goes through the suit array 
+		for (int i = 0; i < suit.length; i++) {
+			// loop goes through the rank array
+			for (int j = 0; j < rank.length; j++) {
+				cardCombo[rank.length * i + j] = suit[i] + rank[j];
 			}
 		}
-		for (int i=0; i<cardCombo.length;i++) {
+//		Printing the CardCombo array
+		for (int i = 0; i < cardCombo.length; i++) {
 			System.out.println(cardCombo[i]);
 		}
+		return cardCombo;
 	}
-	
+
 	public static void main(String[] args) {
-		setup();
+		String[] suit = { "Clubs", "Hearts", "Diamonds", "Spade" };
+		String[] rank = { "Ace", "Kings", "Queen", "Jack", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
+		String[] cardCombo = new String[52];
+		// calling the setup method to create the deck
+		setup(suit, rank, cardCombo);
 
 	}
 
